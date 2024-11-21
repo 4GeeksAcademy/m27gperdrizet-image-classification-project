341db0accaedd57e3e24f79527d0884c0b8e4316
George Perdrizet <george@perdrizet.org>
2024-11-21 14:43:36 -0500

    Updated data download helper script.

--- get_data.sh
+++ get_data.sh
@@ -2,9 +2,9 @@
 
 source .env
 kaggle competitions download dogs-vs-cats -p data/
-unzip dogs-vs-cats.zip
-unzip data/train.zip
-unzip data/test1.zip
+unzip dogs-vs-cats.zip -d data/
+unzip data/train.zip -d data/
+unzip data/test1.zip -d data/
 mkdir data/train/dog
 mkdir data/train/cat
 mv data/train/dog.* data/train/dog/
